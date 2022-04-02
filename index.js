23961341336703bfa989fe62be85e828dba6e02c
Vojtech Vondra <vojtech.vondra@productboard.com>
2022-04-02 10:26:37 +0200

    Reset Heroku starter

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
